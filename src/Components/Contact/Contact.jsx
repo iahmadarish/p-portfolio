36a8183aa652cbcde665bbfa99c39069c10e353e
iahmadarish <ishaqnbsb@gmail.com>
2024-04-25 23:09:27 +0600

    last update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
 import './Contact.css' 
+import {MdOutlineEmail} from 'react-icons/md'
+import { FaFacebookMessenger } from "react-icons/fa";
+import {BsWhatsapp} from 'react-icons/bs' 
+import {useRef} from 'react'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+  emailjs
+  .sendForm('service_jigy09o', 'template_io2cmsr', form.current, {
+    publicKey: 'UD9Y7kuisDSLCOPFT',
+  })
+  .then(
+    () => {
+      console.log('SUCCESS!');
+    },
+    (error) => {
+      console.log('FAILED...', error.text);
+    },
+  );
+};
     return (
-    <section id='contact'>Contact</section>
+      <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact me</h2>
+      <div className="container contact_container" >
+          <div className="contact_options">
+              <article className="contact_option">
+                  <MdOutlineEmail className="contact_option-icon" />
+                  <h4>Email</h4>
+                  <h5>im-ishaq.bd@gmail.com</h5>
+                  <a target="_blank" href="meilto:im.ishaq.bd@gmail.com">Send a Message</a>
+              </article>
+                    <article className="contact_option">
+                  <FaFacebookMessenger className="contact_option-icon"  />
+                  <h4>Messenger</h4>
+                  <h5>Ahmad Ishaq</h5>
+                  <a target="_blank" href="https://www.facebook.com/ishaqmoonira/">Send a Message</a>
+              </article>
+                    <article className="contact_option">
+                  <BsWhatsapp className="contact_option-icon"  />
+                  <h4>Whats App</h4>
+                  <h5>01960465052</h5>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone+01960465052">Send a Message</a>
+              </article>
+          </div>
+          <form ref={form} onSubmit={sendEmail}>
+              <input type="text" name="name" placeholder="Your full name" required/>
+              <input type="email" name="email" placeholder="Your email" required/>
+              <textarea name="message" rows="7" placeholder="Your message" required >
+              </textarea>
+              <button type="submit" className="btn btn_primary"> Send Message </button>
+          </form>
+      </div>
+  </section>
   )
 }
 
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import './Footer.css'
+
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer_logo'> ISHAQ</a>
+          <ul className='permalinks'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#service">Service</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#testimonial">Testimonial</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          <div className="footer_social">
+            <a href="https://www.facebook.com/ishaqmoonira/"><FaFacebook /></a>
+            <a href="https://www.facebook.com/ishaqmoonira/"><FaInstagram /></a>
+            <a href="https://www.facebook.com/ishaqmoonira/"><FaLinkedin /></a>
+          </div>
+          <div className="footer_copyright">
+            <small>All right reserve by ishaq</small>
+          </div>
+    </footer>
+
   )
 }
 
