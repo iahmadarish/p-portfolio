d8134331ba75493563ce94658bafebe78b8100fd
iahmadarish <ishaqnbsb@gmail.com>
2024-04-26 07:55:59 +0600

    update

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a className='btn' href={cv} download>Download CV</a>
-        <a className='btn btn-primary' href="#Contact">Lets talk</a>
+        <a className='btn btn-primary' href="#contact">Lets talk</a>
     </div>
   )
 }
